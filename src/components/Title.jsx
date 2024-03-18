dac720c4c15096a0ab05d2156042bbd04958c088
clark <biancake0803@gmail.com>
2024-03-18 14:39:01 +0800

    title

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -3,7 +3,9 @@ import LiveClockUpdate from "./LiveClock";
 export default function Title() {
   return (
     <>
-      <h1 className="text-dark text-6xl font-medium mb-10">TODO APP </h1>
+      <h1 className="text-dark text-6xl font-medium mb-10 text-center">
+        {" MARINELL'S TODO APP "}
+      </h1>
       <LiveClockUpdate />
     </>
   );
